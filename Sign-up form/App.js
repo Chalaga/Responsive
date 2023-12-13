ed270f40220a2cfdabada7031b841e35aa5c5e4e
Chalaga <150872963+Chalaga@users.noreply.github.com>
2023-12-13 16:04:59 +0400

    Working on the JS

--- Sign-up form/App.js
+++ Sign-up form/App.js
@@ -1,23 +1,28 @@
-const firstname =  document.querySelector ('#firsname');
-const lastname =  document.querySelector ('#lastname');
-const email =  document.querySelector ('#email');
-const Password =  document.querySelector ('#Password');
-const submit = document.querySelector( 'button');
+const firstname = document.querySelector('#firsname');
+const lastname = document.querySelector('#lastname');
+const email = document.querySelector('#email');
+const Password = document.querySelector('#Password');
+const submit = document.querySelector('.freetrial');
+const text = document.querySelector(".text")
+const form = document.querySelector("form")
 
-function validateEmail(email) {
-    var regex = /^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(email);
-}
+// function validateEmail(email) {
+// }
 
-function validatePassword(password) {
-    var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    return regex.test(password);
-}
+// function validatePassword(password) {
+//     let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+//     return regex.test(password);
+// }
 
-submit.addEventListener('click', () => {
-    if (validateEmail(email.value)) {
-        text.innerText = "";
-    } else {
+
+form.addEventListener('click', (e) => {
+    let regex = /^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!regex.test(email.value)) {
         text.innerText = "Looks like this is not an email";
+        e.preventDefault()
+    } else {
+        text.innerText = "";
     }
 });
+
+
