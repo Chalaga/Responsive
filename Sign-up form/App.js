d64e66ba21cd6b71fabc1402e802c38caff5ad00
Chalaga <150872963+Chalaga@users.noreply.github.com>
2023-12-13 17:10:30 +0400

    deleted all the JS

--- Sign-up form/App.js
+++ Sign-up form/App.js
@@ -1,28 +1,16 @@
-const firstname = document.querySelector('#firsname');
+const firstname = document.querySelector('#firstname');
+const firstnametext = document.querySelector(".firstnametext")
+
 const lastname = document.querySelector('#lastname');
+const lastnametext = document.querySelector(".lastnametext")
+
 const email = document.querySelector('#email');
+const emailtext = document.querySelector(".emailtext")
+
 const Password = document.querySelector('#Password');
+const Passwordtext = document.querySelector(".passwordtext")
+
 const submit = document.querySelector('.freetrial');
-const text = document.querySelector(".text")
 const form = document.querySelector("form")
-
-// function validateEmail(email) {
-// }
-
-// function validatePassword(password) {
-//     let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-//     return regex.test(password);
-// }
-
-
-form.addEventListener('click', (e) => {
-    let regex = /^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!regex.test(email.value)) {
-        text.innerText = "Looks like this is not an email";
-        e.preventDefault()
-    } else {
-        text.innerText = "";
-    }
-});
 
 
