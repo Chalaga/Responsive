44c8f78140441b1ee0654791414fd811814380f9
Chalaga <150872963+Chalaga@users.noreply.github.com>
2024-01-08 02:10:13 +0400

    Desktop navbar is done

--- WeatherProject/Mobile.js
+++ WeatherProject/Mobile.js
@@ -1,11 +1,11 @@
-const navbar = document.querySelector('.burgermenu')
-const menu = document.querySelector('index.html')
-const closenavbar = document.querySelector('.backmenu')
+// const navbar = document.querySelector('.burgermenu')
+// const menu = document.querySelector('index.html')
+// const closenavbar = document.querySelector('.backmenu')
 
-navbar.addEventListener('click', ()=>{
-    window.location.href = 'Navbar.html'
-})
+// navbar.addEventListener('click', ()=>{
+//     window.location.href = 'Navbar.html'
+// })
 
-closenavbar.addEventListener('click', ()=>{
-    window.location.href = 'Index.HTML'
-})
+// closenavbar.addEventListener('click', ()=>{
+//     window.location.href = 'Index.HTML'
+// })
