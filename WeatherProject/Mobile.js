31392542002c32adfb9fd6d38b422c773a614f3b
Chalaga <150872963+Chalaga@users.noreply.github.com>
2024-01-08 00:35:39 +0400

    Add navigation bar

---
+++ WeatherProject/Mobile.js
@@ -0,0 +1,11 @@
+const navbar = document.querySelector('.burgermenu')
+const menu = document.querySelector('index.html')
+const closenavbar = document.querySelector('.backmenu')
+
+navbar.addEventListener('click', ()=>{
+    window.location.href = 'Navbar.html'
+})
+
+closenavbar.addEventListener('click', ()=>{
+    window.location.href = 'Index.HTML'
+})
