581079267b310797dff474a5e52eed958fe47a15
Chalaga <150872963+Chalaga@users.noreply.github.com>
2024-01-10 02:56:53 +0400

    Left to add horoscope page and 25daysforcast

--- WeatherProject/main.js
+++ WeatherProject/main.js
@@ -1,10 +1,3 @@
-let title = document.querySelectorAll(".title")
-let desktopDate = document.querySelector(".desktopDate")
-let time = document.querySelector(".time")
-let main = document.querySelector(".main")
-let burgermenu = document.querySelector(".burgermenu")
-let backmenu = document.querySelector(".backmenu")
-
 const data = {
     "city": "Tbilisi",
     "current_weather": {
@@ -147,20 +140,64 @@ const data = {
     ]
 }
 
-let date = Object.values(Object.values(Object.values(data)[1])[2])
+let title = document.querySelectorAll(".title")
+let desktopDate = document.querySelector(".desktopDate")
+let time = document.querySelector(".time")
+let main = document.querySelector(".main")
+let burgermenu = document.querySelector(".burgermenu")
+let backmenu = document.querySelector(".backmenu")
+let celsius = document.querySelectorAll(".celsius")
+let feels = document.querySelectorAll(".feels")
+let humid = document.querySelectorAll(".humid")
+let dewpoint = document.querySelectorAll(".dewpoint")
+let visibility = document.querySelectorAll(".visibility")
+let sunrise = document.querySelectorAll(".sunrise")
+let sunset = document.querySelectorAll(".sunset")
+
+let date = Object.values(data.current_weather.date)
 desktopDate.append(date[1] + " " + date[0])
 time.append(date[2])
 
-let h1 = title[0]
-let p = title[1]
+let title1 = title[0]
+let title2 = title[1]
+title1.append(data.city)
+title2.append(data.city)
 
-h1.append(Object.values(data)[0])
-p.append(Object.values(data)[0])
+let celsius1 = celsius[0]
+let celsius2 = celsius[1]
+celsius1.append(data.current_weather.temperature)
+celsius2.append(data.current_weather.temperature)
 
-console.log(Object.values(data));
+let feels1 = feels[0]
+let feels2 = feels[1]
+feels1.append(data.current_weather.feels_like)
+feels2.append(data.current_weather.feels_like)
 
-console.log(main);
-console.log(burgermenu);
+let humid1 = humid[0]
+let humid2 = humid[1]
+humid1.append(data.current_weather.humidity)
+humid2.append(data.current_weather.humidity)
+
+let dewpoint1 = dewpoint[0]
+let dewpoint2 = dewpoint[1]
+dewpoint1.append("The dew point is " + data.current_weather.dew_point + " right now")
+dewpoint2.append("The dew point is " + data.current_weather.dew_point + " right now")
+
+let visibility1 = visibility[0]
+let visibility2 = visibility[1]
+visibility1.append(data.current_weather.visibility)
+visibility2.append(data.current_weather.visibility)
+
+let sunrise1 = sunrise[0]
+let sunrise2 = sunrise[1]
+
+sunrise1.append(data.current_weather.sunrise)
+sunrise2.append(data.current_weather.sunrise)
+
+let sunset1 = sunset[0]
+let sunset2 = sunset[1]
+sunset1.append(data.current_weather.sunset)
+sunset2.append(data.current_weather.sunset)
 
 burgermenu.addEventListener("click", () => {
     main.classList.add("active")
@@ -168,5 +205,8 @@ burgermenu.addEventListener("click", () => {
 
 backmenu.addEventListener("click", () => {
     main.classList.remove("active")
-
+    
 })
+
+console.log(data);
+console.log(data.current_weather.humidity);
